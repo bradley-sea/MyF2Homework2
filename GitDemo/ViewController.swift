1ae8668e842301a262249520d7445ef3bb134b40
bradleypj@icloud.com <bradleypj@icloud.com>
2015-02-19 20:57:59 -0800

    first commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Bradley Johnson on 2/19/15.
+//  Copyright (c) 2015 BPJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //did my homework
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
